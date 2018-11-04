ffa82d2f50423951c0e9d81252844cb50c865a30
Tom PoLáKoSz <polakosz@freemail.hu>
2018-11-04 15:58:18 +0100

    Extracts sample code from the library to a new Project

---
+++ samples/Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using PoLaKoSz.SMF.Scraper.Themes.Metin2HungaryNet;
+using PoLaKoSz.SMF.Scraper.Workers;
+using System;
+
+namespace PoLaKoSz.SMF.Scraper.Samples.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var theme = new BlackStormTheme();
+            var forumRootUrl = new Uri("http://metin2hungary.dev/");
+
+            var scrapper = new NonAuthenticated(forumRootUrl, theme);
+            scrapper.Run();
+
+            Console.WriteLine("Press any key to continue ...");
+            Console.Read();
+        }
+    }
+}
--- src/SMF-Scraper/Program.cs
+++ src/SMF-Scraper/Program.cs
@@ -1,6 +1,4 @@
-﻿using PoLaKoSz.SMF.Scraper.Themes.Metin2HungaryNet;
-using PoLaKoSz.SMF.Scraper.Workers;
-using System;
+﻿using System;
 
 namespace PoLaKoSz.SMF.Scraper
 {
@@ -8,14 +6,6 @@ namespace PoLaKoSz.SMF.Scraper
 	{
         static void Main(string[] args)
         {
-            var theme        = new BlackStormTheme();
-            var forumRootUrl = new Uri("http://metin2hungary.dev/");
-
-            var scrapper = new NonAuthenticated(forumRootUrl, theme);
-            scrapper.Run();
-
-            Console.WriteLine("Press any key to continue ...");
-            Console.Read();
 
         }
     }
